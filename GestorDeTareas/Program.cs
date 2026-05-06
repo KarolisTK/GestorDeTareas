27b895ae08ca5d31c830344f3f6b5007f6088037
Karolis Tamosaitis Kembre <ktamosaxlv@gmail.com>
2026-05-06 12:20:17 +0200

    feat: Añadir fichero de configuracion para Docker  Se ha añadido un fichero de configuración docker para poder realizar una puesta en producción

--- GestorDeTareas/Program.cs
+++ GestorDeTareas/Program.cs
@@ -39,6 +39,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         };
     });
 builder.Services.AddAuthorization();
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
